74d39a923517251a4f1f51deb89521d39bd3ee13
Okwuosa victor <victorudenna@gmail.com>
2022-09-28 14:42:52 +0100

    created all files

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import {React, Fragment} from 'react'
+
+const NavBar = () => {
+  return (
+<Fragment>
+    FFFJFJFKJFK
+</Fragment>
+  )
+}
+
+export default NavBar
---
+++ src/Pages/Home.js
@@ -0,0 +1,13 @@
+
+import {React, Fragment} from 'react';
+import { NavBar } from '../Components';
+
+const Home = () => {
+  return (
+<Fragment>
+<NavBar/>
+</Fragment>
+  )
+}
+
+export default Home

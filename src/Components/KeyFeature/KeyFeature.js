a6c2d169d0c23438cf72742902c745cc6ef9f87a
victor okwuosa <victorudenna@gmail.com>
2022-10-07 01:04:35 +1300

    Created Testemonial components

--- src/Components/KeyFeature/KeyFeature.js
+++ src/Components/KeyFeature/KeyFeature.js
@@ -1,10 +1,11 @@
 import React, { Fragment } from "react";
-import { IoMdCard } from "react-icons/io";
-import { BiCircle } from "react-icons/bi";
-import { FaBalanceScaleLeft } from "react-icons/fa";
-import { IoIosPaper } from "react-icons/io";
+// import { IoMdCard } from "react-icons/io";
+// import { BiCircle } from "react-icons/bi";
+// import { FaBalanceScaleLeft } from "react-icons/fa";
+// import { IoIosPaper } from "react-icons/io";
+import { FeatureCard } from "../../Components";
 
-const Card = () => {
+const KeyFeature = () => {
   return (
     <Fragment>
       <div className="px-2 md:px-6 xl:px-navx pb-20 md:mt-20 md:pb-30">
@@ -16,14 +17,32 @@ const Card = () => {
             Beautiful Websites
           </p>
         </div>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-          <div className="py-4 px-4 rounded-xl drop-shadow-xl bg-white pb-10">
+          <FeatureCard
+            title="Modular"
+            description="All components are built to be used in any combination."
+          />
+          <FeatureCard
+            title="Responsive"
+            description="Quick is optimized to work for most devices."
+          />
+          <FeatureCard
+            title="Scalable"
+            description="Remain consistent while developing new features."
+          />
+          <FeatureCard
+            title="Customizable"
+            description="Change a few variables and the whole theme adapts."
+          />
+          {/* <div className="py-4 px-4 rounded-xl drop-shadow-xl bg-white pb-10">
             <div className="py-1"><IoMdCard size='30' fill="indigo"/></div>
             <div>
               <h3 className="text-2xl">Modular</h3>
               <p className="text-gray">All components are built to be used in any combination.</p>
             </div>
           </div>
+
           <div className="py-4 px-4 rounded-xl drop-shadow-xl bg-white pb-10">
             <div className="py-1"><BiCircle size='30' fill="indigo"/></div>
             <div>
@@ -31,6 +50,7 @@ const Card = () => {
               <p className="text-gray">Quick is optimized to work for most devices.</p>
             </div>
           </div>
+
           <div className="py-4 px-4 rounded-xl drop-shadow-xl bg-white pb-10">
             <div className="py-1"><FaBalanceScaleLeft size='30' fill="indigo"/></div>
             <div>
@@ -38,17 +58,18 @@ const Card = () => {
               <p className="text-gray">Remain consistent while developing new features.</p>
             </div>
           </div>
+
           <div className="py-4 px-4 rounded-xl drop-shadow-xl bg-white pb-10">
             <div className="py-1"><IoIosPaper size='30' fill="indigo"/></div>
             <div>
               <h3 className="text-2xl">Customizable</h3>
               <p className="text-gray">Change a few variables and the whole theme adapts.</p>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </Fragment>
   );
 };
 
-export default Card;
+export default KeyFeature;
